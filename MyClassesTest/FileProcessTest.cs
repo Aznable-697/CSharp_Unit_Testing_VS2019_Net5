e32bc7aee3c4c893a247e860679b4d4c8b03f67d
Matt W <mattwhiteker@gmail.com>
2022-05-06 23:09:40 -0400

    Adding Attributes to Help Organize Unit Tests

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -24,11 +24,13 @@ namespace MyClassesTest
             //TODO: Clean up after all tests in Class
 
         }
-
+        
         [TestInitialize]
         public void TestInitialize()
         {
             TestContext.WriteLine("In TestInitialize() method");
+
+            WriteDescription(this.GetType());
 
             if (TestContext.TestName.StartsWith("FileNameDoesExist"))
             {
@@ -59,6 +61,37 @@ namespace MyClassesTest
         }
 
         [TestMethod]
+        [DataRow(1, 1, DisplayName = "First Test (1,1)")]
+        [DataRow(42, 42, DisplayName = "Second Test (42,42)")]
+        public void AreNumbersEqual(int num1, int num2)
+        {
+            Assert.AreEqual(num1, num2);
+        }
+
+        [TestMethod]
+        [DeploymentItem("FileToDeploy.txt")]
+        [DataRow(@"C:\Windows\Regedit.exe", DisplayName = "Regedit.exe")]
+        [DataRow("FileToDeploy.txt", DisplayName = "Deployment Item: FileToDeploy")]
+        public void FileNameUsingDataRow(string fileName)
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            if (!fileName.Contains(@"\"))
+            {
+                fileName = TestContext.DeploymentDirectory + @"\" + fileName;
+            }
+            TestContext.WriteLine("Checking File " + fileName);
+            fromCall = fp.FileExists(fileName);
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        [Description("Check to see if a file exists.")]
+        [Owner("MattW")]
+        [Priority(1)]
+        [TestCategory("NoException")]
+        //[Ignore]
         public void FileNameDoesExist()
         {
             FileProcess fp = new FileProcess();
@@ -72,6 +105,17 @@ namespace MyClassesTest
         }
 
         [TestMethod]
+        [Timeout(6000)]
+        public void SimulateTimeout()
+        {
+            System.Threading.Thread.Sleep(8000);
+        }
+
+        [TestMethod]
+        [Description("Check to see if a file does not exists.")]
+        [Owner("KayleeW")]
+        [Priority(2)]
+        [TestCategory("NoException")]
         public void FileNameDoesNotExist()
         {
             FileProcess fp = new FileProcess();
@@ -86,6 +130,10 @@ namespace MyClassesTest
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
+        [Description("Check for a thrown ArgumentNullException using ExpectedExeption.")]
+        [Owner("MattW")]
+        [Priority(3)]
+        [TestCategory("Exception")]
         public void FileNameNullOrEmpty_UsingAttribute()
         {
             FileProcess fp = new FileProcess();
@@ -98,12 +146,17 @@ namespace MyClassesTest
         }
 
         [TestMethod]
+        [Description("Check for a thrown ArgumentNullException using try...catch.")]
+        [Owner("KadenW")]
+        [Priority(3)]
+        [TestCategory("Exception")]
         public void FileNameNullOrEmpty_UsingTryCatch()
         {
             FileProcess fp = new FileProcess();
 
             try
             {
+                TestContext.WriteLine("Checking for a null File");
                 fp.FileExists("");
             }
             catch (ArgumentNullException)
